71edf9f59d76aebd24f3c8168be1b8bba346b1e3
HiredMark <suner@syuleyman.com>
2022-01-17 11:53:48 +0000

    Variables

--- Starter/1.Start/1.Start/Program.cs
+++ Starter/1.Start/1.Start/Program.cs
@@ -12,6 +12,7 @@ namespace Start
     static void Main(string[] args)
     {
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
     }
 }
 }
---
+++ Starter/2.Variables/2.Variables/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Start
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "Gareth";
+            int characterAge = 42;
+            bool isTrainer = true;
+            char surnameInitial = 'D';
+            double percentage = 99.99D;
+
+            Console.WriteLine("There was a man named " + characterName + " " + surnameInitial + ". It is " + isTrainer + " that he was a trainer" + " he was mean " + percentage + "%  of the time.");
+            Console.ReadLine();
+        }
+    }
+}
